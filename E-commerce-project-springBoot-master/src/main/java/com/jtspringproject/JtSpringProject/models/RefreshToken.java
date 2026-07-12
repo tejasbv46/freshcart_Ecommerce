e68b1800dcc0e921a9ff9c875486dbe0c5a52a1c
tejasbv46 <tejasbv46@users.noreply.github.com>
2026-07-12 22:38:20 +0530

    Initial commit - FreshCart Ecommerce

---
+++ E-commerce-project-springBoot-master/src/main/java/com/jtspringproject/JtSpringProject/models/RefreshToken.java
@@ -0,0 +1,55 @@
+package com.jtspringproject.JtSpringProject.models;
+
+import jakarta.persistence.*;
+import java.time.Instant;
+
+@Entity(name = "REFRESH_TOKEN")
+@Table
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Instant getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Instant expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+}
